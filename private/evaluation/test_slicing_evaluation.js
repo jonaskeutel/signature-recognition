92ca0a0d81715990e15ef1ec798ef27da7cc312a
Dominic Sauer <dominic.sauer@yahoo.de>
2016-06-28 21:16:26 +0200

    use signatures from db in order to test slicing

--- private/evaluation/test_slicing_evaluation.js
+++ private/evaluation/test_slicing_evaluation.js
@@ -2,24 +2,24 @@ var dtw_slicing  = require('./dtw_slicing_evaluation.js')
 var db              = require('../database/dbInterface.js')
 var fs              = require('fs')
 
-// db.init()
-//   .then( () => {
-//
-// db.getAllUser()
-//   .then( (user) => {
-//     console.log(user)
-//     db.getSignatures(52)
-//       .then( (signatures_1) => {
-//         db.getSignatures(47)
-//           .then( (signatures_2) => {
-//               dtw_slicing.compare(signatures_2[0], signatures_1, function(result) {
-//                 console.log('finished');
-//               })
-//           })
-//       })
-//     })
-// })
+db.init()
+  .then( () => {
 
-dtw_slicing.compare('abc', 'def', function(result) {
-  console.log(result);
+db.getAllUser()
+  .then( (user) => {
+    console.log(user)
+    db.getSignatures(52)
+      .then( (signatures_1) => {
+        db.getSignatures(47)
+          .then( (signatures_2) => {
+              dtw_slicing.compare(signatures_2[0], signatures_1, function(result) {
+                console.log('finished');
+              })
+          })
+      })
+    })
 })
+
+// dtw_slicing.compare('abc', 'def', function(result) {
+//   console.log(result);
+// })
